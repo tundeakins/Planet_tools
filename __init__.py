572f614a31fb364ba647efc8f2652abd69a58d87
tundeakins <babatundeakinsanmi@gmail.com>
2020-07-24 21:22:06 +0100

    updated init.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from Planet_parameter_conversions import convert_param, calculate_param, some_stats, estimate_effect 
+from Planet_tools import convert_param, calculate_param, some_stats, estimate_effect 
